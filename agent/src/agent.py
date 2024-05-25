3e7eeb5d35c85ef7e735c03b348d0416b586b789
wrongways skidmark <jez@wain.name>
2024-05-25 11:04:26 +0200

    Restructure code

--- agent/src/agent.py
+++ agent/src/agent.py
@@ -1,3 +1,4 @@
+import shlex
 import subprocess
 import threading
 from pathlib import Path
@@ -10,6 +11,92 @@ RAPL_PATH = "/sys/devices/virtual/powercap/intel-rapl/"
 RAPL_SAMPLE_TIME_SECS = .25
 HTTP_202_ACCEPTED = 202
 HTTP_409_CONFLICT = 409
+
+
+# Utility functions
+
+
+def run_command(command):
+    """Executes command as a subprocess, ensuring the language is et to en_US.
+    Returns the stdout & stderr of the command as strings"""
+    return subprocess.run(
+            shlex.split(command),
+            encoding="utf8",
+            text=True,
+            capture_output=True,
+            env={
+                "LANG": "en_US.UTF-8",
+            }
+    )
+
+
+def os_name():
+    """Reads /etc/os-release to determine the OS name
+    NOTE: This routine only works on Linux"""
+
+    os_data = Path("/etc/os-release").read_text().splitlines()
+    os_data = {d[0]: d[1] for d in [
+        e.strip().split('=') for e in os_data
+    ] if len(d) == 2
+               }
+    os_name = (
+            os_data.get("PRETTY_NAME") or
+            f"{os_data.get('NAME', 'Unknown')} {os_data.get('VERSION', '')}"
+    )
+    return {"os_name": os_name.strip('"')}
+
+
+def hostname():
+    """Does what is says on the can - returns the hostname"""
+    return {"hostname": run_command("hostname -s").stdout.strip()}
+
+
+def cpu_info():
+    """Runs lscpu to collect details regarding the installed CPUs"""
+    CPU_KEYS = [
+        "Architecture",
+        "CPU(s)",
+        "Thread(s) per core",
+        "Core(s) per socket",
+        "Socket(s)",
+        "Vendor ID",
+        "Model name",
+        "CPU MHz",
+        "CPU max MHz",
+        "CPU min MHz",
+    ]
+
+    cpu_data = run_command("lscpu").stdout
+    # Transform each line into dictionary entry, split on colon ':'
+    cpu_data = {d[0]: d[1] for d in [line.strip().split(":") for line in cpu_data.splitlines()]}
+    # make all the keys lowercase and replace spaces with underscores
+    return {k.lower().replace(" ", "_"): cpu_data[k].strip() for k in CPU_KEYS}
+
+
+def hw_info():
+    """Returns platform/firmware info"""
+    dmi_path = Path("/sys/devices/virtual/dmi/id")
+    dmi_files = [
+        "bios_date",
+        "bios_vendor",
+        "bios_version",
+        "board_name",
+        "board_vendor",
+        "board_version",
+        "sys_vendor",
+    ]
+    return {
+        f: dmi_path.joinpath(f).read_text().strip() for f in dmi_files
+    }
+
+
+def system_info():
+    return web.json_response(
+            hw_info() |
+            cpu_info() |
+            hostname() |
+            os_name()
+    )
 
 
 class CappingAgent:
@@ -41,6 +128,7 @@ class CappingAgent:
         self.app.add_routes([
             web.get('/rapl_power', self.rapl_power),
             web.post('/firestarter', self.firestarter),
+            web.get('/system_info', system_info)
         ])
         web.run_app(self.app(), port=port_number)
 
--- bmc/src/__init__.py
+++ bmc/src/__init__.py
@@ -1,3 +1,2 @@
-from bmc import BMC, BMC_Type
 from ipmi_bmc import IpmiBMC
 from redfish_bmc import RedfishBMC
