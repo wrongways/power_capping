f8017b8d54b8dcdf9de27dd6fede49cabcfd43b8
wrongways skidmark <jez@wain.name>
2024-06-18 15:54:30 +0200

    Add async call

--- BMC/src/test_bmc.py
+++ BMC/src/test_bmc.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 from ipmi_bmc import IpmiBMC
@@ -7,4 +8,9 @@ bmc_username = os.environ.get('BMC_USERNAME')
 bmc_password = os.environ.get('BMC_PASSWORD')
 ipmi_bmc = IpmiBMC(bmc_hostname=bmc_hostname, bmc_username=bmc_username, bmc_password=bmc_password)
 
-print(f'Current power: {ipmi_bmc.current_power}')
+
+async def main():
+    print(f'Current power: {await ipmi_bmc.current_power}')
+
+
+asyncio.run(main())
