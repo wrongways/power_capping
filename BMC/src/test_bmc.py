fb19f329c768beb92da2e024546eb55643bf2b5a
wrongways skidmark <jez@wain.name>
2024-06-18 16:03:11 +0200

    Add redfish test

--- BMC/src/test_bmc.py
+++ BMC/src/test_bmc.py
@@ -2,15 +2,18 @@ import asyncio
 import os
 
 from ipmi_bmc import IpmiBMC
-
-bmc_hostname = os.environ.get('BMC_HOSTNAME')
-bmc_username = os.environ.get('BMC_USERNAME')
-bmc_password = os.environ.get('BMC_PASSWORD')
-ipmi_bmc = IpmiBMC(bmc_hostname=bmc_hostname, bmc_username=bmc_username, bmc_password=bmc_password)
+from redfish_bmc import RedfishBMC
 
 
 async def main():
-    print(f'Current power: {await ipmi_bmc.current_power}')
+    bmc_hostname = os.environ.get('BMC_HOSTNAME')
+    bmc_username = os.environ.get('BMC_USERNAME')
+    bmc_password = os.environ.get('BMC_PASSWORD')
+    ipmi_bmc = IpmiBMC(bmc_hostname=bmc_hostname, bmc_username=bmc_username, bmc_password=bmc_password)
+    redfish_bmc = RedfishBMC(bmc_hostname=bmc_hostname, bmc_username=bmc_username, bmc_password=bmc_password)
+
+    print(f'IPMI Current power: {await ipmi_bmc.current_power}')
+    print(f'Redfish Current power: {await redfish_bmc.current_power}')
 
 
 asyncio.run(main())
