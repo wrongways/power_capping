0ccde16d7c7e0eb83cc6686015b5b0ec5a977579
wrongways skidmark <jez@wain.name>
2024-06-22 09:08:04 +0200

    Add test_redfish

--- collector/src/test_collector.py
+++ collector/src/test_collector.py
@@ -12,8 +12,21 @@ bmc_password = os.environ.get('BMC_PASSWORD')
 
 async def test_ipmi():
     sleep_time = 10
+    collector = Collector(bmc_hostname, bmc_username, bmc_password, BMC_Type.IPMI, 'http://t3r1nod23:5432',
+                          '/tmp/collector_test.db')
+    collect_thread = threading.Thread(target=asyncio.run, args=(collector.start_collect(),))
+    collect_thread.start()
+    print(f"Started collect thread, sleeping for {sleep_time} seconds")
+    await asyncio.sleep(sleep_time)
+    await collector.end_collect()
+    collect_thread.join()
+
+
+async def test_redfish():
+    sleep_time = 10
     collector = Collector(bmc_hostname, bmc_username, bmc_password, BMC_Type.REDFISH, 'http://t3r1nod23:5432',
                           '/tmp/collector_test.db')
+    await collector.bmc_connect()
     collect_thread = threading.Thread(target=asyncio.run, args=(collector.start_collect(),))
     collect_thread.start()
     print(f"Started collect thread, sleeping for {sleep_time} seconds")
@@ -24,6 +37,7 @@ async def test_ipmi():
 
 async def main():
     await test_ipmi()
+    await test_redfish()
 
 
 asyncio.run(main())
