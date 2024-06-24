c611eb266d8cad8988ff79fe09a9f5a5029fdab3
wrongways skidmark <jez@wain.name>
2024-06-24 11:25:12 +0200

    Fill out the cli

--- runner/src/cli.py
+++ runner/src/cli.py
@@ -16,7 +16,7 @@ def parse_args():
                         help='''Path to the sqlite3 db on the local system holding the collected statistics. \
                         If the file does not exist, it will be created, otherwise the tables will be updated \
                         with the data from this run. If not provided, the database file will be found in the \
-                        current directory, with the name: <agent_name><timestamp>.db
+                        current directory, with the name: <agent_name><timestamp>_capping_test.db
                         ''')
     parser.add_argument('-i', '--ipmitool_path',
                         # required='ipmi' in sys.argv,
@@ -24,5 +24,35 @@ def parse_args():
                         default='/usr/bin/ipmitool',
                         help='Path to ipmitool on the local system. Only required if bmc_type="ipmi". \
                         Default: /usr/bin/ipmitool')
+
+    parser.add_argument('--min_load', type=int, default=100, choices=range(1, 101),
+                        help='Minimum firestarter load for test run')
+
+    parser.add_argument('--max_load', type=int, default=100, choices=range(2, 101),
+                        help='Maximum firestarter load for test run')
+
+    parser.add_argument('--load_delta', type=int, default=0, choices=range(101),
+                        help='The change in firestarter load between each test run.')
+
+    parser.add_argument('--min_threads', type=int, default=0,
+                        help='Minimum firestarter threads for test run')
+
+    parser.add_argument('--max_threads', type=int, default=100,
+                        help='Maximum firestarter threads for test run')
+
+    parser.add_argument('--threads_delta', type=int, default=0,
+                        help='The change in firestarter threads between each test run.')
+
+    parser.add_argument('--cap_min', type=int, required=True,
+                        help='Minimum power cap setting')
+
+    parser.add_argument('--cap_max', type=int, required=True,
+                        help='Maximum power cap setting')
+
+    parser.add_argument('--cap_delta', type=int, required=True,
+                        help='The change in cap settings for each test run. \
+                        The test runner will generate a run for each step between min and max')
+
+    parser.add_argument('--cap_direction', choices=['up', 'down', 'updown', 'both'])
 
     return parser.parse_args()
