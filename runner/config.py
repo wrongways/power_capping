cd0ee709d9eb8d06862b104c1ccd8e3e2e0753c6
wrongways skidmark <jez@wain.name>
2024-06-30 13:02:40 +0200

    Add cli config params

--- runner/config.py
+++ runner/config.py
@@ -1,5 +1,18 @@
-class TestConfig:
-    warmup_seconds = 15
-    per_step_runtime_seconds = 30
-    inter_step_pause_seconds = 5
-    uncapped_power = 1400
+runner_config: {
+    'bmc_type': None,
+    'agent_url': None,
+    'db_path': None,
+    'ipmitool_path': None,
+
+    'warmup_seconds': 15,
+    'per_step_runtime_seconds': 30,
+    'inter_step_pause_seconds': 5,
+    'uncapped_power': 1400,
+
+    'min_load': 90,
+    'max_load': 100,
+    'load_delta': 5,
+    'cap_min': 350,
+    'cap_max': 600,
+    'cap_delta': 400,
+}
