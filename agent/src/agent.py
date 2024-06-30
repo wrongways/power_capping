9fb8799edd2a7812ba2d02a792f5a58a1ba4f2d7
wrongways skidmark <jez@wain.name>
2024-06-30 23:36:58 +0200

    Rename

--- agent/src/agent.py
+++ agent/src/agent.py
@@ -1,21 +1,12 @@
-"""Capping Agent.
-
-Runs an aiohttp web server offering 3 services:
-
-- get system information
-- get rapl power readings
-- post launch firestarter
-
-The agent has its own CLI
-"""
-
 import argparse
 import json
 import logging
 import subprocess
 import threading
+from asyncio import sleep
 from pathlib import Path
-from time import monotonic_ns, sleep
+from pprint import pprint
+from time import monotonic_ns
 
 from aiohttp import web
 
@@ -26,149 +17,138 @@ RAPL_SAMPLE_TIME_SECS = .25
 HTTP_202_ACCEPTED = 202
 HTTP_409_CONFLICT = 409
 
+logging.basicConfig(level='DEBUG')
 logger = logging.getLogger(__name__)
 
-class CappingAgent:
-    def __init__(self, port_number: int, firestarter_path: str):
-        """
-        Params:
-            firestarter_path: str - the filepath to the firestarter executable
-        """
-        self.firestarter_path = firestarter_path
-        self.firestarter_thread = None
-        # Get the list of packages/sockets
-        packages_paths = list(Path(RAPL_PATH).glob('intel-rapl:[0-9]*'))
-        logger.debug(f'{packages_paths=}')
+firestarter_thread = None
+firestarter_path = ''
 
-        # For each package, create a dictionary with the energy file path,
-        # max_energy_range_uj and package name
-        self.package_info = {
-            path: {
-                'energy_uj_path': path.joinpath('energy_uj'),
-                'name': path.joinpath('name').read_text().strip(),
-                'max_energy': self.read_energy_path(path, read_max_energy=True)
-            }
-            for path in packages_paths
+
+async def get_system_info(_request):
+    return web.json_response(system_info())
+
+
+def read_energy_path(path, read_max_energy=False):
+    """
+    Reads a RAPL energy file. If max is true, reads the max_energy_range_uj file.
+    Otherwise, read the energy_uj file.
+
+    Params:
+        path: the path to the package directory
+        read_max_energy: flag if true, read max_energy_range_uj, otherwise energy_uj
+    Returns:
+        The value of the file as an integer
+    """
+    energy_path = (
+        path.joinpath('max_energy_range_uj') if read_max_energy
+        else package_info[path]['energy_uj_path']
+    )
+    return int(energy_path.read_text().strip())
+
+
+async def rapl_power(_request):
+    """Calculates the current socket power consumption for all sockets.
+
+    For each socket, read the current energy value, sleep for a short period,
+    then re-read the energy value. Power is then energy delta divided by time
+    delta.
+
+    Params:
+        none
+    Returns:
+        JSON list of power consumption
+    """
+    # Get the initial energy readings
+    start_values = {
+        path: {
+            'energy_uj': read_energy_path(path),
+            'timestamp': monotonic_ns()
         }
-        logger.debug(f'package_info:\n{json.dumps(self.package_info, indent=3, sort_keys=True)}')
-        self.app = web.Application()
+        for path in package_info
+    }
 
-        self.app.add_routes([
-            web.get('/rapl_power', self.rapl_power),
-            web.post('/firestarter', self.firestarter),
-            web.get('/system_info', self.get_system_info)
-        ])
-        web.run_app(self.app(), port=port_number)
+    # Wait a while for energy to be consumed
+    await sleep(RAPL_SAMPLE_TIME_SECS)
 
-    async def rapl_power(self, _request):
-        """
-        Calculates the current socket power consumption for all sockets.
+    # Build a dictionary of powers, keyed on package name.
+    package_powers = {}
+    for path, reading in start_values.items():
+        package_name = package_info[path]['name']
+        start_energy = reading['energy_uj']
+        start_timestamp = reading['timestamp']
+        end_energy = read_energy_path(path)
+        end_timestamp = monotonic_ns()
 
-        For each socket, read the current energy value, sleep for a short period,
-        then re-read the energy value. Power is then energy delta divided by time
-        delta.
-
-        Params:
-            none
-        Returns:
-            JSON list of power consumption
-        """
-
-        # Get the initial energy readings
-        start_values = {
-            path: {
-                'energy_uj': self.read_energy_path(path),
-                'timestamp': monotonic_ns()
-            }
-            for path in self.package_info
-        }
-
-        # Wait a while for energy to be consumed
-        sleep(RAPL_SAMPLE_TIME_SECS)
-
-        # Build a dictionary of powers, keyed on package name.
-        package_powers = {}
-        for path, reading in start_values.items():
-            package_name = self.package_info[path]['name']
-            start_energy = reading['energy_uj']
-            start_timestamp = reading['timestamp']
-            end_energy = self.read_energy_path(path)
-            end_timestamp = monotonic_ns()
-
-            # Check for wrap-around
-            energy_delta = (
-                end_energy - start_energy if end_energy > start_energy
-                else self.package_info[path]['max_energy'] - start_energy + end_energy
-            )
-
-            # The power of each package/socket = delta_energy / delta_time
-            time_delta_ns = end_timestamp - start_timestamp
-            package_power_watts = energy_delta / time_delta_ns * 1000
-            package_powers[package_name] = package_power_watts
-
-        return web.json_response(package_powers)
-
-    async def firestarter(self, request: web.Request):
-        """"/firestarter route handler.
-
-        @param request - the request object provided by aiohttp
-        """
-
-        # If firestarter already running return 409 - conflict
-        if self.firestarter_thread is not None and self.firestarter_thread.is_alive():
-            return web.json_response({'error': 'Firestarter already running'}, status=HTTP_409_CONFLICT)
-
-        # 'join()' any previous thread. Given that the thread must be complete
-        # at this point (see previous check), then this will return immediately.
-
-        if self.firestarter_thread is not None:
-            self.firestarter_thread.join()
-
-        # pull out the request arguments
-        json_body = await request.json()
-        self.firestarter_thread = threading.Thread(target=self.launch_firestarter, args=[json_body], name='Firestarter')
-        self.firestarter_thread.start()
-        return web.json_response(None, status=HTTP_202_ACCEPTED)
-
-    def launch_firestarter(self, args):
-        """Launches the firestarter subprocess.
-
-        @param args: {str: int} - list of firestarter arguments
-            timeout → runtime_secs
-            load → pct_load
-            threads → n_threads
-        """
-
-        runtime_secs = args.get('runtime_secs', 30)
-        pct_load = args.get('pct_load', 100)
-        n_threads = args.get('n_threads', 0)
-        command_line = f'{self.firestarter_path} --quiet --timeout {runtime_secs} --load {pct_load} --threads {n_threads}'
-
-        # Launch the subprocess, sending the firestarter banner to /dev/null
-        subprocess.run(command_line.split(), stdout=subprocess.DEVNULL)
-
-    def read_energy_path(self, path, read_max_energy=False):
-        """\
-        Reads a RAPL energy file. If max is true, reads the max_energy_range_uj file.
-        Otherwise, read the energy_uj file
-
-        Params:
-            path: the path to the package directory
-            read_max_energy: flag if true, read max_energy_range_uj, otherwise energy_uj
-        Returns:
-            The value of the file as an integer
-        """
-        energy_path = (
-            path.joinpath('max_energy_range_uj') if read_max_energy
-            else self.package_info[path]['energy_uj_path']
+        # Check for wrap-around
+        energy_delta = (
+            end_energy - start_energy if end_energy > start_energy
+            else package_info[path]['max_energy'] - start_energy + end_energy
         )
-        return int(energy_path.read_text().strip())
+
+        # The power of each package/socket = delta_energy / delta_time
+        time_delta_ns = end_timestamp - start_timestamp
+        package_power_watts = energy_delta / time_delta_ns * 1000
+        package_powers[package_name] = package_power_watts
+
+    return web.json_response(package_powers)
 
 
-    async def get_system_info(self):
-        return web.json_response(system_info())
+def launch_firestarter(args):
+    """Launches the firestarter subprocess.
 
-if __name__ == '__main__':
+    @param args: {str: int} - list of firestarter arguments
+        timeout → runtime_secs
+        load → pct_load
+        threads → n_threads
+    """
+
+    print(f"Firestarter call: {args}")
+    runtime_secs = args.get('runtime_secs', 30)
+    pct_load = args.get('pct_load', 100)
+    n_threads = args.get('n_threads', 0)
+    command_line = f'{firestarter_path} --quiet --timeout {runtime_secs} --load {pct_load} --threads {n_threads}'
+    logger.debug(f"Firestarter command: {command_line}")
+    print(f"Firestarter command: {command_line}")
+
+    # Launch the subprocess, sending the firestarter banner to /dev/null
+    subprocess.run(command_line.split(), stdout=subprocess.DEVNULL)
+
+
+async def firestarter(request: web.Request):
+    """"/firestarter route handler.
+
+    @param request - the request object provided by aiohttp
+    """
+    # If firestarter already running return 409 - conflict
+    global firestarter_thread
+
+    if firestarter_thread is not None and firestarter_thread.is_alive():
+        return web.json_response({'error': 'Firestarter already running'}, status=HTTP_409_CONFLICT)
+
+    # 'join()' any previous thread. Given that the thread must be complete
+    # at this point (see previous check), then this will return immediately.
+
+    if firestarter_thread is not None:
+        firestarter_thread.join()
+
+    # pull out the request arguments
+    json_body = await request.json()
+    print("Request args:", json.dumps(json_body, indent=3, sort_keys=True))
+    pprint(json_body)
+
+    firestarter_thread = threading.Thread(target=launch_firestarter, args=[json_body],
+                                          name='Firestarter')
+    firestarter_thread.start()
+    return web.json_response(None, status=HTTP_202_ACCEPTED)
+
+
+async def shutdown(_request):
+    await app.shutdown()
+    await app.cleanup()
+    return web.json_response(None, status=HTTP_202_ACCEPTED)
+
+
+def parse_cli():
     parser = argparse.ArgumentParser(
             prog='CappingAgent',
             description='Launches the capping tool agent',
@@ -182,12 +162,44 @@ if __name__ == '__main__':
     )
     parser.add_argument(
             '-f', '--firestarter',
-            default='/home_nfs/wainj/local/bin/firestarter',
+            default='/tmp/FIRESTARTER',
             help='Fully qualified path to the firestarter load generation programme'
     )
 
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('-V', '--version', action='version')
     args = parser.parse_args()
-    agent = CappingAgent(port_number=args.port, firestarter_path=args.firestarter)
-    web.run_app(agent.app)
+
+    return args
+
+
+if __name__ == '__main__':
+    packages_paths = list(Path(RAPL_PATH).glob('intel-rapl:[0-9]*'))
+    package_info = {
+        path: {
+            'energy_uj_path': path.joinpath('energy_uj'),
+            'name': path.joinpath('name').read_text().strip(),
+            'max_energy': read_energy_path(path, read_max_energy=True)
+        } for path in packages_paths
+    }
+    # logger.debug(f'{packages_paths=}')
+    # str_package_info = {str(k): str(v) for k, v in package_info}
+    # logger.debug(f'package_info:\n{json.dumps(str_package_info, indent=3, sort_keys=True)}')
+
+    firestarter_thread = None
+
+    cli_args = parse_cli()
+    firestarter_path = cli_args.firestarter
+    if not Path(firestarter_path).exists():
+        msg = f"{firestarter_path} Not found"
+        logger.error(msg)
+        raise ValueError(msg)
+
+    app = web.Application()
+    app.add_routes([web.get('/system_info', get_system_info),
+                    web.get('/rapl_power', rapl_power),
+                    web.post('/firestarter', firestarter),
+                    web.post('/shutdown', shutdown)],
+                   )
+
+    web.run_app(app, port=cli_args.port)
